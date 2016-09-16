ec3a9d812923533b05696263530b3f0db5e23416
ekonovalova <elizaveta.konovalova@hotmail.com>
2016-09-16 17:03:00 +1000

    - Added job search on the dashboard page - Able to retrieve data from job search

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -8,7 +8,7 @@ var app = express();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var databasePostgres = req.app.locals.dataPostgres;
-
+  GetAllJobs(databasePostgres);
   databasePostgres.query('SELECT * FROM jobs ORDER BY census_start_date ASC, job_name ASC', function (err, response) {
     if(err) throw err;
     app.locals.dataJobs = jsonquery('',{data:response.rows}).value;
@@ -21,9 +21,17 @@ router.post('/', function (req, res, next) {
   if(req.body.clientname != null){
     databasePostgres.query('SELECT * FROM jobs WHERE job_name= $1::text', [req.body.clientname], function (err, response) {
       if(err) throw err;
-      res.render('dashboard', {data: app.locals.dataJobs, modalData:response.rows, moment:moment});
+      app.locals.searchJobs = response.rows;
+      res.render('dashboard', {data: app.locals.dataJobs, modalData: app.locals.searchJobs, moment:moment});
     });
   }
 });
+
+function GetAllJobs(database){
+  database.query('SELECT * FROM jobs', function (err, response) {
+    if(err) throw err;
+    app.locals.searchJobs = response.rows;
+  });
+}
 
 module.exports = router;
